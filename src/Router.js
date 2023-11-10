7deb0f97b380b6ec264a6b51159b178509237801
David Tong <87665198+DavidTHW@users.noreply.github.com>
2023-11-10 19:12:53 +0800

    change color theme
    remove some codes added new components

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,12 @@
 import {Route, Routes } from "react-router"
 import Landing from "./containers/Landing"
+import Series from "./containers/Series";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" Component={Landing}/>
+      <Route path="/series" Component={Series} />
     </Routes>
   );
 }
---
+++ src/containers/Series.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import SerialDramas from '../components/SerialDramas'
+import axios from 'axios'
+
+const Series = () => {
+
+const seriesUrl = "http://localhost:8080/api/v1/movies"
+
+  const [series , setSeries] = useState()
+
+  const getAllSeries = async () => {
+    await axios.get(seriesUrl)
+    .then((res) => setSeries(res.data))
+    .catch((err) => console.log(err))
+  }
+  
+  useEffect(()=> {
+    getAllSeries()
+  },[])
+  console.log(series)
+
+  return (
+    <div>
+      <Header/>
+      <div className="body">
+      <SerialDramas series={series}/>
+      </div>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Series
